b3c439010bf1b010bcb230b5ab4b9cfccf35ecb9
Martin <martinpfatrisch@gmail.com>
2023-06-29 16:47:26 +0200

    Added drawing the snake head

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,50 @@ static ALLOC: WeeAlloc = WeeAlloc::INIT;
 #[wasm_bindgen]
 pub struct World {
     width: usize,
+    snake: Snake,
 }
 
 #[wasm_bindgen]
 impl World {
     pub fn new() -> World {
-        World { width: 8 }
+        World {
+            width: 8,
+            snake: Snake::from(10),
+        }
+    }
+
+    pub fn from(width: usize) -> World {
+        World {
+            width,
+            snake: Snake::from(10),
+        }
     }
 
     pub fn width(&self) -> usize {
         self.width
+    }
+
+    pub fn snake_head_idx(&self) -> usize {
+        self.snake.body[0].0
+    }
+}
+
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+}
+
+impl Snake {
+    fn new() -> Snake {
+        Snake {
+            body: vec![SnakeCell(10)],
+        }
+    }
+
+    fn from(spawn_index: usize) -> Snake {
+        Snake {
+            body: vec![SnakeCell(spawn_index)],
+        }
     }
 }
