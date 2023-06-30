873467a0d318d3c110090882403130f33995490e
Martin <martinpfatrisch@gmail.com>
2023-06-30 09:00:19 +0200

    Switched to typescript

--- www/index.ts
+++ www/index.ts
@@ -1,30 +1,31 @@
 import init, { World } from 'snake_game';
 
-await init();
-const CELL_SIZE = 32;
+init().then(_ => {
 
-const world = World.from(16);
-const canvas = document.getElementById('snake-canvas');
-const ctx = canvas.getContext('2d');
-
-const worldWidth = world.width();
-
-canvas.height = worldWidth * CELL_SIZE;
-canvas.width = worldWidth * CELL_SIZE;
-
-const drawWorld = () => {
-  ctx.beginPath();
-
-  for (let x = 0; x < worldWidth + 1; x++) {
-    ctx.moveTo(CELL_SIZE * x, 0);
+  const CELL_SIZE = 32;
+  
+  const world = World.from(16);
+  const canvas = <HTMLCanvasElement>document.getElementById('snake-canvas');
+  const ctx = canvas.getContext('2d');
+  
+  const worldWidth = world.width();
+  
+  canvas.height = worldWidth * CELL_SIZE;
+  canvas.width = worldWidth * CELL_SIZE;
+  
+  const drawWorld = () => {
+    ctx.beginPath();
+    
+    for (let x = 0; x < worldWidth + 1; x++) {
+      ctx.moveTo(CELL_SIZE * x, 0);
     ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE);
   }
-
+  
   for (let y = 0; y < worldWidth + 1; y++) {
     ctx.moveTo(0, CELL_SIZE * y);
     ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y);
   }
-
+  
   ctx.stroke();
 };
 
@@ -32,7 +33,7 @@ const drawSnake = () => {
   const snakeIdx = world.snake_head_idx();
   const col = snakeIdx % worldWidth;
   const row = Math.floor(snakeIdx / worldWidth);
-
+  
   ctx.beginPath();
   ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
   ctx.stroke();
@@ -46,13 +47,15 @@ const paint = () => {
 const update = () => {
   setTimeout(() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+    
     world.update();
     paint();
-
+    
     requestAnimationFrame(update);
   }, 100);
 };
 
 paint();
 update();
+
+});
