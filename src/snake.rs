c003184a23ed7d35f57aa003a127c74f10456fb0
Martin <martinpfatrisch@gmail.com>
2023-06-30 11:23:28 +0200

    Refactoring of rust file structure

---
+++ src/snake.rs
@@ -0,0 +1,28 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+pub struct SnakeCell(pub usize);
+
+pub struct Snake {
+    pub body: Vec<SnakeCell>,
+    pub direction: Direction,
+}
+
+impl Snake {
+    pub fn from(spawn_index: usize, direction: Direction, size: usize) -> Snake {
+        let mut body = vec![];
+
+        for i in 0..size {
+            body.push(SnakeCell(spawn_index - i))
+        }
+
+        Snake { body, direction }
+    }
+}
